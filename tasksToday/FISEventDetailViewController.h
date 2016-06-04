81894a2c0b99b2d008982b6075a78e1c7617ea01
John Richardson <richardsonjm@gmail.com>
2016-06-03 21:30:56 -0400

    Add ui view controller for event details

---
+++ tasksToday/FISEventDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISEventDetailViewController.h
+//  tasksToday
+//
+//  Created by John Richardson on 6/3/16.
+//  Copyright © 2016 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISEventDetailViewController : UIViewController
+
+@end
