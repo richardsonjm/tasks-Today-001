17205fcf810a130d219152a8176c97a3c13cde3b
John Richardson <richardsonjm@gmail.com>
2016-06-03 21:37:48 -0400

    Copy calender method implementations

--- tasksToday/FISViewController.m
+++ tasksToday/FISViewController.m
@@ -30,15 +30,57 @@
 }
 
 - (NSArray *)getNextYearsEvents {
-    return @[];
+
+    EKEventStore *store = [[EKEventStore alloc] init];
+    // Get the appropriate calendar
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+
+    // Create the end date components
+    NSDateComponents *oneYearFromNowComponents = [[NSDateComponents alloc] init];
+    oneYearFromNowComponents.year = 1;
+    NSDate *oneYearFromNow = [calendar dateByAddingComponents:oneYearFromNowComponents
+                                                       toDate:[NSDate date]
+                                                      options:0];
+
+    // Create the predicate from the event store's instance method
+    NSPredicate *predicate = [store predicateForEventsWithStartDate:[NSDate date]
+                                                            endDate:oneYearFromNow
+                                                          calendars:nil];
+
+    // Fetch all events that match the predicate
+    NSArray *events = [store eventsMatchingPredicate:predicate];
+    return events;
 }
 
 - (NSArray *)getLastYearsEvents {
-    return @[];
+    EKEventStore *store = [[EKEventStore alloc] init];
+    // Get the appropriate calendar
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+
+    // Create the start date components
+
+    // Create the end date components
+    NSDateComponents *oneYearFromNowComponents = [[NSDateComponents alloc] init];
+    oneYearFromNowComponents.year = -1;
+    NSDate *oneYearFromNow = [calendar dateByAddingComponents:oneYearFromNowComponents
+                                                       toDate:[NSDate date]
+                                                      options:0];
+
+    // Create the predicate from the event store's instance method
+    NSPredicate *predicate = [store predicateForEventsWithStartDate:oneYearFromNow
+                                                            endDate:[NSDate date]
+                                                          calendars:nil];
+
+    // Fetch all events that match the predicate
+    NSArray *events = [store eventsMatchingPredicate:predicate];
+    return events;
 }
 
 - (void)requestCalendarPermission {
-    
+    EKEventStore *store = [[EKEventStore alloc] init];
+    [store requestAccessToEntityType:EKEntityTypeEvent completion:^(BOOL granted, NSError *error) {
+
+    }];
 }
 
 
